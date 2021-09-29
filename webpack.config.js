2a3c03443f88bb440a567e367b663ca28887a6cb
Mauricio G <mgarzon@celerik.com>
2021-09-29 10:14:26 -0500

    feat(project): initial config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  target: "web",
+  mode: "development",
+  devServer: {
+      port: 3001,
+  },
+  output: {
+    path: path.resolve("build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("public", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/styles.css",
+    }),
+  ],
+};
