a819611452fa65325a2232100a896385d9ef75b1
Mauricio G <mgarzon@celerik.com>
2021-11-03 09:32:20 -0500

    feat(project): adds commitizen and some config changes
    adds commitizen, changes dependencies versions and jest testEnv to jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
